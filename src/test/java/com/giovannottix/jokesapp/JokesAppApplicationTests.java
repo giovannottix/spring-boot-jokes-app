852743a2f22e00ea43456c61612c73844fe4cbf0
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-03 19:37:00 -0500

    Initial commit

---
+++ src/test/java/com/giovannottix/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovannottix.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
